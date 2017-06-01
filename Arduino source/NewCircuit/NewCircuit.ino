b8220a6f5b3b4b66180c6d18762f3b42c58f8c91
Daniela Brozzoni <brozzoni@live.it>
2017-06-01 09:51:53 +0200

    Add new circuit code #2

--- Arduino source/NewCircuit/NewCircuit.ino
+++ Arduino source/NewCircuit/NewCircuit.ino
@@ -54,9 +54,9 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 
-  //pinMode(0, OUTPUT);
-  //pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -68,15 +68,15 @@ void loop()
   lcd.setCursor(0,0);
   Serial.println(analogSensor);
 
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(2, HIGH);
-/*
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+
   if(analogSensor > warning && analogSensor < sensorThres)
   {
-    digitalWrite(0, LOW);
-    digitalWrite(1, HIGH);
     digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
     
     lcd.setCursor(0,1);
     lcd.print("ALERT!");
@@ -84,14 +84,14 @@ void loop()
 
   else if(analogSensor >= sensorThres)
   {
-    digitalWrite(0, HIGH);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
     
     lcd.setCursor(0,1);
     lcd.print("WARNING!");    
   }
-*/
+
   delay(1000);
 }
 
